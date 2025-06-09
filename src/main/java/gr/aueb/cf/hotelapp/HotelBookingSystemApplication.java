0c9c2fad4dbd9b32f8e8c83a0be61ce24987e4d7
Ioannis Michos <giannismichos004@gmail.com>
2025-06-09 18:46:44 +0300

    Initial project setup with Gradle and base structure

---
+++ src/main/java/gr/aueb/cf/hotelapp/HotelBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.hotelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingSystemApplication.class, args);
+	}
+
+}
