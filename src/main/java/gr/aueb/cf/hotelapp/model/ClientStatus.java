7d3e3dfbd0843c4520d50f1acb41a3a8f75d2fe4
Ioannis Michos <giannismichos004@gmail.com>
2025-06-10 12:24:02 +0300

    Add ClientStatus and EmployeeStatus entity classes

---
+++ src/main/java/gr/aueb/cf/hotelapp/model/ClientStatus.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.hotelapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "client_status")
+public class ClientStatus {
+
+    @Id
+    private Long clientId;
+
+    @Column(name = "total_bookings")
+    private int totalBookings;
+
+    @Column(name = "discount_active")
+    private boolean discountActive;
+}
---
+++ src/main/java/gr/aueb/cf/hotelapp/model/EmployeeStatus.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.hotelapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "employee_status")
+public class EmployeeStatus {
+
+    @Id
+    private Long userId;
+
+    @Column(name = "total_bookings")
+    private int totalBookings;
+
+    @Column(name = "bonus_awarded")
+    private boolean bonusAwarded;
+
+    @Column(name = "last_updated")
+    private LocalDateTime lastUpdated;
+}
