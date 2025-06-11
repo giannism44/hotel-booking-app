dd0f1837a788cbe43fa1a63b7aebc0c6c92cd19f
Ioannis Michos <giannismichos004@gmail.com>
2025-06-11 19:42:32 +0300

    git commit -m "Create EmployeeMapper for converting between Employee entity and DTOs"

---
+++ src/main/java/gr/aueb/cf/hotelapp/mapper/EmployeeMapper.java
@@ -0,0 +1,50 @@
+package gr.aueb.cf.hotelapp.mapper;
+
+import gr.aueb.cf.hotelapp.core.enums.Role;
+import gr.aueb.cf.hotelapp.dto.*;
+import gr.aueb.cf.hotelapp.model.Client;
+import gr.aueb.cf.hotelapp.model.Employee;
+import gr.aueb.cf.hotelapp.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmployeeMapper {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public EmployeeReadOnlyDTO mapToEmployeeReadOnlyDTO(Employee employee) {
+        return new EmployeeReadOnlyDTO(
+                employee.getId(),
+                employee.getFirstname(),
+                employee.getLastname(),
+                employee.getUser().getUsername()
+        );
+    }
+
+    public Employee mapToEmployeeEntity(EmployeeInsertDTO dto) {
+        Employee employee = new Employee();
+        employee.setFirstname(dto.firstname());
+        employee.setLastname(dto.lastname());
+
+        User user = new User();
+        user.setUsername(dto.username());
+        user.setPassword(passwordEncoder.encode(dto.password()));
+        user.setRole(Role.EMPLOYEE);
+
+        employee.setUser(user);
+
+        return employee;
+    }
+
+    public Employee mapToEmployeeEntity(EmployeeUpdateDTO dto) {
+        Employee employee = new Employee();
+        employee.setId(dto.id());
+        employee.setFirstname(dto.firstname());
+        employee.setLastname(dto.lastname());
+
+        return employee;
+    }
+}
