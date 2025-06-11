ee8b9fb3c23dd6c841437126a1e5f15abe606e7c
Ioannis Michos <giannismichos004@gmail.com>
2025-06-11 21:48:11 +0300

    git commit -m "Create ReservationMapper for converting between Employee entity and DTOs"

---
+++ src/main/java/gr/aueb/cf/hotelapp/mapper/ReservationMapper.java
@@ -0,0 +1,38 @@
+package gr.aueb.cf.hotelapp.mapper;
+
+import gr.aueb.cf.hotelapp.core.enums.ReservationStatus;
+import gr.aueb.cf.hotelapp.dto.ReservationInsertDTO;
+import gr.aueb.cf.hotelapp.dto.ReservationReadOnlyDTO;
+import gr.aueb.cf.hotelapp.model.Client;
+import gr.aueb.cf.hotelapp.model.Reservation;
+import gr.aueb.cf.hotelapp.model.Room;
+import gr.aueb.cf.hotelapp.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReservationMapper {
+
+    public ReservationReadOnlyDTO mapToReservationReadOnlyDTO(Reservation reservation) {
+        String clientFullName = reservation.getClient().getFirstname() + " " + reservation.getClient().getLastname();
+
+        return new ReservationReadOnlyDTO(
+                reservation.getId(),
+                reservation.getCheckIn(),
+                reservation.getCheckOut(),
+                reservation.getStatus(),
+                reservation.getRoom().getRoomNumber(),
+                clientFullName
+        );
+    }
+
+    public Reservation mapToReservationEntity(ReservationInsertDTO dto, Room room, Client client, User createdBy) {
+        Reservation reservation = new Reservation();
+        reservation.setCheckIn(dto.checkIn());
+        reservation.setCheckOut(dto.checkOut());
+        reservation.setRoom(room);
+        reservation.setClient(client);
+        reservation.setCreatedBy(createdBy);
+        reservation.setStatus(ReservationStatus.PENDING);
+        return reservation;
+    }
+}
