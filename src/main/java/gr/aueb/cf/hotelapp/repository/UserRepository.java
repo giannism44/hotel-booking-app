dc3b257b9280f3bda29b8c3ce08efa39f08ba0fb
Ioannis Michos <giannismichos004@gmail.com>
2025-06-11 16:01:47 +0300

    git commit -m "Refactor UserRepository: return List of roles instead of Optional"

--- src/main/java/gr/aueb/cf/hotelapp/repository/UserRepository.java
+++ src/main/java/gr/aueb/cf/hotelapp/repository/UserRepository.java
@@ -1,12 +1,13 @@
-package gr.aueb.cf.hotelapp.repotistory;
+package gr.aueb.cf.hotelapp.repository;
 
 import gr.aueb.cf.hotelapp.core.enums.Role;
 import gr.aueb.cf.hotelapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
-    Optional<User> findAllByRole(Role role);
+    List<User> findAllByRole(Role role);
 }
