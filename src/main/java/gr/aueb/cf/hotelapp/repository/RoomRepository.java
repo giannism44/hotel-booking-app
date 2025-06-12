2060a035ee425c5461d4858b17bfdcd14a4ddb13
Ioannis Michos <giannismichos004@gmail.com>
2025-06-12 21:03:55 +0300

    git commit -m "Add Optional findByRoomNumber query to RoomRepository"

--- src/main/java/gr/aueb/cf/hotelapp/repository/RoomRepository.java
+++ src/main/java/gr/aueb/cf/hotelapp/repository/RoomRepository.java
@@ -3,5 +3,8 @@ package gr.aueb.cf.hotelapp.repository;
 import gr.aueb.cf.hotelapp.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    Optional<Room> findByRoomNumber(String roomNumber);
 }
