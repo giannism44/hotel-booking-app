3f23d2c0bc765a9cb00bd833dedfd69063c68abc
Ioannis Michos <giannismichos004@gmail.com>
2025-06-12 16:17:37 +0300

    git commit -m "Add Optional findByVat query to ClientRepository"

--- src/main/java/gr/aueb/cf/hotelapp/repository/ClientRepository.java
+++ src/main/java/gr/aueb/cf/hotelapp/repository/ClientRepository.java
@@ -3,5 +3,8 @@ package gr.aueb.cf.hotelapp.repository;
 import gr.aueb.cf.hotelapp.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByVat(String vat);
 }
