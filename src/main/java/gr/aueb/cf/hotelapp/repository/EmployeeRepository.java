353b522a15b8378a174739d00e1bd6fb685f1a6a
Ioannis Michos <giannismichos004@gmail.com>
2025-06-13 19:07:14 +0300

    git commit -m "Add Optional findByUser query to EmployeeRepository"

--- src/main/java/gr/aueb/cf/hotelapp/repository/EmployeeRepository.java
+++ src/main/java/gr/aueb/cf/hotelapp/repository/EmployeeRepository.java
@@ -1,7 +1,11 @@
 package gr.aueb.cf.hotelapp.repository;
 
 import gr.aueb.cf.hotelapp.model.Employee;
+import gr.aueb.cf.hotelapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByUser(User user);
 }
