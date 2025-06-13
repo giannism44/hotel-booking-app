5884cfcaa6e4bc781cf826c66aae81129daae442
Ioannis Michos <giannismichos004@gmail.com>
2025-06-13 19:12:04 +0300

    git commit -m "Add PredefinedRoom enum with room data and RoomDataLoader to seed database"

---
+++ src/main/java/gr/aueb/cf/hotelapp/init/RoomDataLoader.java
@@ -0,0 +1,43 @@
+package gr.aueb.cf.hotelapp.init;
+
+import gr.aueb.cf.hotelapp.core.enums.RoomType;
+import gr.aueb.cf.hotelapp.model.Room;
+import gr.aueb.cf.hotelapp.repository.RoomRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RoomDataLoader {
+
+    private final RoomRepository roomRepository;
+
+    //@PostConstruct
+    public void initRooms() {
+        for (int floor = 1; floor <= 5; floor++) {
+            for (int roomNum = 1; roomNum <= 10; roomNum++) {
+                String roomNumber = floor + String.format("%02d", roomNum);
+
+                if (roomRepository.findByRoomNumber(roomNumber).isEmpty()) {
+                    Room room = new Room();
+                    room.setRoomNumber(roomNumber);
+
+                    if (floor == 5) {
+                        room.setRoomType(RoomType.SUITE);
+                        room.setPrice(150.0);
+                    } else if (floor >= 3) {
+                        room.setRoomType(RoomType.DOUBLE);
+                        room.setPrice(100.0);
+                    } else {
+                        room.setRoomType(RoomType.SINGLE);
+                        room.setPrice(75.0);
+                    }
+
+                    room.setIsAvailable(true);
+                    roomRepository.save(room);
+                }
+            }
+        }
+    }
+}
