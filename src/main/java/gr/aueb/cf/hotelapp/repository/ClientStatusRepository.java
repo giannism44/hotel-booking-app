95d15e3d0da718d3423cd8bf77199c476129e32a
Ioannis Michos <giannismichos004@gmail.com>
2025-06-13 19:07:44 +0300

    git commit -m "Add Optional findByClientId query to ClientStatusRepository"

--- src/main/java/gr/aueb/cf/hotelapp/repository/ClientStatusRepository.java
+++ src/main/java/gr/aueb/cf/hotelapp/repository/ClientStatusRepository.java
@@ -3,5 +3,8 @@ package gr.aueb.cf.hotelapp.repository;
 import gr.aueb.cf.hotelapp.model.ClientStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientStatusRepository extends JpaRepository<ClientStatus,Long> {
+    Optional<ClientStatus> findByClientId(Long clientId);
 }
