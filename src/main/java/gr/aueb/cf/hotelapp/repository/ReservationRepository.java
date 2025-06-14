958cf796af01a41f7bee70562383d38b14d13497
Ioannis Michos <giannismichos004@gmail.com>
2025-06-14 18:19:35 +0300

    git commit -m "Add existsByRoomAndCheckInLessThanEqualAndCheckOutGreaterThanEqual query to ReservationRepository"

--- src/main/java/gr/aueb/cf/hotelapp/repository/ReservationRepository.java
+++ src/main/java/gr/aueb/cf/hotelapp/repository/ReservationRepository.java
@@ -1,7 +1,12 @@
 package gr.aueb.cf.hotelapp.repository;
 
 import gr.aueb.cf.hotelapp.model.Reservation;
+import gr.aueb.cf.hotelapp.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    boolean existsByRoomAndCheckInLessThanEqualAndCheckOutGreaterThanEqual(
+            Room room, LocalDate checkOut, LocalDate checkIn);
 }
