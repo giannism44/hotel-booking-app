27f19bce0f2ad34eebac59a3a427b4205206b759
Ioannis Michos <giannismichos004@gmail.com>
2025-06-16 17:33:47 +0300

    git commit -m "Add Implement UserDetails in User entity for Spring Security integration"

--- src/main/java/gr/aueb/cf/hotelapp/model/User.java
+++ src/main/java/gr/aueb/cf/hotelapp/model/User.java
@@ -6,7 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -15,7 +19,7 @@ import java.util.List;
 @Getter
 @Setter
 @Table(name = "users")
-public class User extends AbstractEntity{
+public class User extends AbstractEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,4 +42,29 @@ public class User extends AbstractEntity{
 
     @OneToMany(mappedBy = "createdBy")
     private List<Reservation> reservationsCreated;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
