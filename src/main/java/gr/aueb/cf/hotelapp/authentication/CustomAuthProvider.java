6ba0e864573e15157f68b3afdb809855ff98f617
Ioannis Michos <giannismichos004@gmail.com>
2025-06-18 14:47:58 +0300

    git commit -m "Refactor CustomAuthProvider: remove injected PasswordEncoder field and add passwordEncode method"

--- src/main/java/gr/aueb/cf/hotelapp/authentication/CustomAuthProvider.java
+++ src/main/java/gr/aueb/cf/hotelapp/authentication/CustomAuthProvider.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -15,18 +16,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class CustomAuthProvider {
 
     private final UserDetailsService userDetailsService;
-    private final PasswordEncoder passwordEncoder;
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder);
+        authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(11);
     }
 }
