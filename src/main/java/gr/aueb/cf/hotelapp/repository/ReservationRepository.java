8e7546bd2ff4c368278d64c0cf13539e8005de8c
Ioannis Michos <giannismichos004@gmail.com>
2025-06-19 17:58:18 +0300

    git commit -m "Add Javadoc comments to ReservationRepository"

--- src/main/java/gr/aueb/cf/hotelapp/repository/ReservationRepository.java
+++ src/main/java/gr/aueb/cf/hotelapp/repository/ReservationRepository.java
@@ -6,7 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 
+/**
+ * Repository για οντότητα Reservation.
+ * Περιλαμβάνει μέθοδο για έλεγχο επικαλυπτόμενων κρατήσεων.
+ */
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    /**
+     * Επιστρέφει true αν υπάρχει ήδη κράτηση για το δωμάτιο
+     * που επικαλύπτει το δοσμένο χρονικό διάστημα.
+     */
     boolean existsByRoomAndCheckInLessThanEqualAndCheckOutGreaterThanEqual(
             Room room, LocalDate checkOut, LocalDate checkIn);
 }
