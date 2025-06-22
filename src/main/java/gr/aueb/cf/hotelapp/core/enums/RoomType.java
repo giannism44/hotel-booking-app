a593359a868f6b7c585f05338b0dc051f37d6198
Ioannis Michos <giannismichos004@gmail.com>
2025-06-22 19:57:56 +0300

    git commit -m "Add Javadoc comments to RoomType"

--- src/main/java/gr/aueb/cf/hotelapp/core/enums/RoomType.java
+++ src/main/java/gr/aueb/cf/hotelapp/core/enums/RoomType.java
@@ -1,5 +1,9 @@
 package gr.aueb.cf.hotelapp.core.enums;
 
+/**
+ * Τύποι δωματίων του ξενοδοχείου.
+ * Χρησιμοποιούνται για την κατηγοριοποίηση των διαθέσιμων δωματίων.
+ */
 public enum RoomType {
     SINGLE,
     DOUBLE,
