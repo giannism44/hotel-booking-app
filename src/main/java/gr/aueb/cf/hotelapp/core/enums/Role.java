6b1b13bf173f03ef70e9ea4591513336cfe81480
Ioannis Michos <giannismichos004@gmail.com>
2025-06-22 19:57:15 +0300

    git commit -m "Add Javadoc comments to Role"

--- src/main/java/gr/aueb/cf/hotelapp/core/enums/Role.java
+++ src/main/java/gr/aueb/cf/hotelapp/core/enums/Role.java
@@ -1,5 +1,9 @@
 package gr.aueb.cf.hotelapp.core.enums;
 
+/**
+ * Ρόλοι χρηστών στο σύστημα.
+ * Χρησιμοποιούνται για τον καθορισμό δικαιωμάτων πρόσβασης (Spring Security).
+ */
 public enum Role {
     CLIENT,
     EMPLOYEE
