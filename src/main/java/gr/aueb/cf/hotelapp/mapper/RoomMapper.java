dbf808a591a53acf99079d85611bcd88552c285e
Ioannis Michos <giannismichos004@gmail.com>
2025-06-22 20:03:44 +0300

    git commit -m "Add Javadoc comments to RoomMapper"

--- src/main/java/gr/aueb/cf/hotelapp/mapper/RoomMapper.java
+++ src/main/java/gr/aueb/cf/hotelapp/mapper/RoomMapper.java
@@ -5,6 +5,10 @@ import gr.aueb.cf.hotelapp.dto.RoomUpdateDTO;
 import gr.aueb.cf.hotelapp.model.Room;
 import org.springframework.stereotype.Component;
 
+/**
+ * Mapper για μετατροπή μεταξύ Room entity και DTOs.
+ * Χρησιμοποιείται για προβολή και ενημέρωση δωματίων.
+ */
 @Component
 public class RoomMapper {
 
