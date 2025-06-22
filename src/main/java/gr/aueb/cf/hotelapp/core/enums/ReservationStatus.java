8e05371efa8d3a78996436447db9ecbecca3455e
Ioannis Michos <giannismichos004@gmail.com>
2025-06-22 19:56:45 +0300

    git commit -m "Add Javadoc comments to ReservationStatus"

--- src/main/java/gr/aueb/cf/hotelapp/core/enums/ReservationStatus.java
+++ src/main/java/gr/aueb/cf/hotelapp/core/enums/ReservationStatus.java
@@ -1,5 +1,10 @@
 package gr.aueb.cf.hotelapp.core.enums;
 
+
+/**
+ * Καταστάσεις κράτησης.
+ * Χρησιμοποιείται για να δηλώνεται αν μια κράτηση είναι επιβεβαιωμένη ή ακυρωμένη.
+ */
 public enum ReservationStatus {
     CONFIRMED,
     CANCELLED
