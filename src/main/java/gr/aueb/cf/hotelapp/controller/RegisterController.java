2e047023de076aac70d36ac1252b1b9682500cd0
Ioannis Michos <giannismichos004@gmail.com>
2025-06-24 17:41:01 +0300

    git commit -m "Add RegisterController with GET /register endpoint for role selection page"

---
+++ src/main/java/gr/aueb/cf/hotelapp/controller/RegisterController.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.hotelapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register";
+    }
+}
