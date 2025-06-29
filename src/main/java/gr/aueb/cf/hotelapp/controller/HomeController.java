eb2b44347fd3e1238872118ce2e764aaed10a3ce
Ioannis Michos <giannismichos004@gmail.com>
2025-06-29 17:18:42 +0300

    git commit -m "Merge Register and Login mappings into HomeController and set home and login redirect"

--- src/main/java/gr/aueb/cf/hotelapp/controller/HomeController.java
+++ src/main/java/gr/aueb/cf/hotelapp/controller/HomeController.java
@@ -3,21 +3,33 @@ package gr.aueb.cf.hotelapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String index() {
-        return "home";
+        return "pages/home";
     }
 
     @GetMapping("/about")
     public String about() {
-        return "about";
+        return "pages/about";
     }
 
     @GetMapping("/management")
     public String managementPage() {
         return "management";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "pages/register";
+    }
+
+    @GetMapping("/login")
+    public String login(Principal principal) {
+        return (principal == null) ? "pages/loginb" : "redirect:/dashboard";
     }
 }
