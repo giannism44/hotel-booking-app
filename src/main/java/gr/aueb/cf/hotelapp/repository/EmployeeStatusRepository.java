56472ecc46ebf06d53290a74a51bec1eb56290fc
Ioannis Michos <giannismichos004@gmail.com>
2025-07-01 15:28:15 +0300

    git commit -m "Delete EmployeeStatus.java, EmployeeStatusRepository.java"

--- src/main/java/gr/aueb/cf/hotelapp/repository/EmployeeStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package gr.aueb.cf.hotelapp.repository;
-
-import gr.aueb.cf.hotelapp.model.EmployeeStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeStatusRepository extends JpaRepository<EmployeeStatus, Long> {
-}
