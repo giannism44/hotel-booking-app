b8c29d03826f56d2b355f921191549417e7d5b40
Ioannis Michos <giannismichos004@gmail.com>
2025-07-07 23:47:30 +0300

    git commit -m "Remove UserReadOnlyDTO and UserInsertDTO classes"

--- src/main/java/gr/aueb/cf/hotelapp/dto/UserReadOnlyDTO.java
+++
@@ -1,9 +0,0 @@
-package gr.aueb.cf.hotelapp.dto;
-
-import gr.aueb.cf.hotelapp.core.enums.Role;
-
-public record UserReadOnlyDTO(
-        Long id,
-        String username,
-        Role role
-) {}
